860e19ffa1a1d3cc0da1c9918680905e79ed7a1a
Phrygien <84671612+phrygien@users.noreply.github.com>
2023-05-16 12:06:54 +0200

    liste client avec montant total de vente GF-28

--- app/Http/Livewire/LivClient.php
+++ app/Http/Livewire/LivClient.php
@@ -1,13 +1,199 @@
 <?php
 
 namespace App\Http\Livewire;
-
+use App\Models\Client;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class LivClient extends Component
 {
+    use WithPagination;
+    public $isLoading, $client_id, $nom, $raison_sociale, $adresse;
+    public $afficherListe=true;
+    public $createClient=false;
+    public $editClient=false;
+    public $notification =false; 
+    public $confirmUpdate = false; 
+    public $recordToDelete;
+    public $btnCreate = true;
+    protected $paginationTheme = 'bootstrap';
+
     public function render()
     {
-        return view('livewire.liv-client');
+        $clients = Client::select('clients.nom', 'clients.raison_sociale', 'clients.adresse')
+        ->leftJoin('sortie_poulets', 'clients.id', '=', 'sortie_poulets.id_client')
+        ->selectRaw('clients.*, SUM(sortie_poulets.nombre * sortie_poulets.prix_unite) as total_montant')
+        ->groupBy('clients.id')
+        ->paginate(10);
+
+        return view('livewire.liv-client', [
+            'clients' => $clients
+        ]);
     }
+
+    public function formClient()
+    {
+        $this->isLoading = true;
+        $this->createClient =true;
+        $this->afficherListe = false;
+        $this->btnCreate = false;
+        $this->isLoading = false;
+    }
+
+    public function resetInput()
+    {
+        $this->nom = '';
+        $this->raison_sociale = '';
+        $this->adresse = '';
+        $this->resetValidation();
+    }
+
+    public function saveClient()
+    {
+        $this->isLoading = true;
+        $data = $this->validate([
+            'nom' => 'required|unique:clients,nom',
+            'raison_sociale' => 'required',
+            'adresse' => 'required'
+        ]);
+
+        try{
+
+        Client::create($data);
+        $this->notification = true;
+        session()->flash('message', 'Client bien enregistré!');
+        $this->resetValidation();
+        $this->resetInput();
+        $this->isLoading = false;
+        }catch(\Exception $e){
+            session()->flash('message', $e->getMessage());
+        }
+
+    }
+
+    public function cancelCreate()
+    {
+        $this->isLoading = true;
+        $this->createClient = false;
+        $this->afficherListe = true;
+        $this->resetInput();
+        $this->resetValidation();
+        $this->btnCreate = true;
+        $this->isLoading = false;
+    }
+
+    public function editClient($id)
+    {
+        $this->isLoading = true;
+
+        $client = Client::findOrFail($id);
+        $this->nom = $client->nom;
+        $this->raison_sociale = $client->raison_sociale;
+        $this->adresse = $client->adresse;
+        $this->client_id = $id;
+        $this->editClient = true;
+        $this->createClient = false;
+        $this->btnCreate = false;
+        $this->isLoading = false;
+        $this->afficherListe = false;
+    }
+
+    public function removeNotification()
+    {
+        $this->dispatchBrowserEvent('removeNotification');
+    }
+
+    public function hideNotification()
+    {
+        $this->notification = false;
+    }
+
+
+    public function confirmerUpdate()
+    {
+        $this->confirmUpdate = true;
+    }
+
+    public function updateClient()
+    {
+        $this->isLoading = true;
+
+        $this->validate([
+            'nom' => 'required|unique:clients,nom,' . $this->client_id,
+            'raison_sociale' => 'required',
+            'adresse' => 'required'
+        ]);
+
+        try{
+
+            $client = Client::findOrFail($this->client_id);
+            $client->update([
+                'nom' => $this->nom,
+                'raison_sociale' => $this->raison_sociale,
+                'adresse' => $this->adresse,
+            ]);
+
+            session()->flash('message', 'Modification avec sucée');
+            $this->editClient = false;
+            $this->notification = true;
+            $this->resetInput();
+            $this->resetValidation();
+            $this->confirmUpdate = false;
+            $this->afficherListe = true;
+            $this->btnCreate = true;
+
+            $this->isLoading = false;
+        }catch(\Exception $e){
+
+        }
+
+    }
+
+    public function cancelModal()
+    {
+        $this->isLoading = true;
+
+        $this->confirmUpdate = false;
+        $this->editClient = true;
+
+        $this->isLoading = false;
+    }
+
+    public function cancelUpdate()
+    {
+        $this->isLoading = true;
+
+        $this->confirmUpdate = false;
+        $this->editClient = false;
+        $this->resetInput();
+        $this->resetValidation();
+        $this->btnCreate = true;
+        $this->afficherListe = true;
+        $this->isLoading = false;
+    }
+
+    public function confirmerDelete($id)
+    {
+        $this->recordToDelete = Client::findOrFail($id);
+    }
+
+    public function cancelDelete()
+    {
+        $this->recordToDelete = null;
+    }
+
+    public function delete()
+    {
+        try{
+        $this->recordToDelete->delete();
+        $this->recordToDelete = null;
+        $this->notification = true;
+        session()->flash('message', 'Suppression avec sucée');
+    }catch(\Exception $e){
+            //$this->notification = true;
+            session()->flash('error', 'Impossible de supprimer le client. Il est déja utilisé !');
+        }
+
+    }
+
 }
--- resources/views/livewire/liv-client.blade.php
+++ resources/views/livewire/liv-client.blade.php
@@ -1,3 +1,145 @@
 <div>
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
 </div>
+
+<div class="row mb-4">
+    @if($notification)
+    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-4">
+        <div id="notification" wire:transition.fade.out.500ms>
+        @if (session()->has('message'))
+            <div class="alert alert-success" role="alert">
+                <i class="icon-info1"></i>{{ session('message')}}
+            </div>
+        @endif
+    </div>
+    <div wire:poll.5s="hideNotification"></div>
+    </div>
+    @endif
+
+    @if($createClient)
+        @include('livewire.clients.create')
+    @endif
+
+    @if($editClient)
+        @include('livewire.clients.edit')
+    @endif
+
+    @if ($afficherListe)
+    <div class="col-md-12 mb-3">
+        <div class="card text-left">
+
+            <div class="card-body">
+                <h4 class="card-title mb-3">{{ __('Liste clients ') }}</h4>
+                <p>
+                    <button class="btn btn-primary btn-rounded" wire:click="formClient" wire:loading.attr="disabled" wire:target="formClient">
+                        <span wire:loading.remove wire:target="formClient">Créer client</span>
+                        <span wire:loading wire:target="formClient">
+                            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                            création...
+                        </span>
+                    </button>
+                </p>
+                <div class="table-responsive">
+                                    
+                @if($recordToDelete)
+                <!-- CSS -->
+                <style>
+                    .overlay {
+                    position: fixed;
+                    top: 0;
+                    left: 0;
+                    bottom: 0;
+                    right: 0;
+                    background-color: rgba(0, 0, 0, 0.5);
+                    z-index: 9999;
+                    }
+                    
+                    .centered {
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                    }
+                </style>
+  
+                <!-- HTML -->
+                <div class="overlay">
+                    <div class="centered">
+                    <div class="alert alert-warning text-center">
+                        <strong class="text-black">Suppression client !</strong>
+                        <p class="text-black">Vous etes sure de supprimer le client : {{$recordToDelete->nom }}?</p>
+                        @if (session()->has('error'))
+                        <div class="alert alert-warning border-danger" role="alert">
+                            <i class="icon-info1"></i>{{ session('error')}}
+                        </div>
+                        @endif
+                        <p class="text-center">
+                            <button class="btn btn-secondary btn-rounded" wire:click="cancelDelete()">{{ __('Annuler') }}</button>
+                            <button class="btn btn-danger btn-rounded" wire:click="delete()">{{ __('Supprimer') }}</button>
+                        </p>
+                    </div>
+                    </div>
+                </div>
+  
+                <script>
+                    // Désactiver le clic sur le reste de la page
+                    document.querySelector('.overlay').addEventListener('click', function(e) {
+                    e.stopPropagation();
+                    });
+                </script>
+                @endif
+
+                    <table class="table table-striped table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">{{ __('Nom')}}</th>
+                                <th scope="col">{{ __('Raison sociale')}}</th>
+                                <th scope="col">{{ __('Adresse')}}</th>
+                                <th scope="col">{{ __('Montant total de vente')}}</th>
+                                <th scope="col" width="149px">{{ __('Actions')}}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($clients as $client)
+                            <tr>
+                                <td>{{ $client->nom }}</td>
+                                <td>{{ $client->raison_sociale }}</td>
+                                <td>{{ $client->adresse }}</td>
+                                <td><span class="text-success text-14">{{ $client->total_montant  }}</span> (Ar)</td>
+                                <td>
+                                    <button wire:click="editType({{$client->id }})" wire:loading.attr="disabled" wire:target="editType({{$client->id }})" class="btn btn-raised btn-rounded btn-raised-primary">
+                                        <span wire:loading.remove wire:target="editType({{$client->id }})"><i class="nav-icon i-Pen-2 font-weight-bold"></i></span>
+                                        <span wire:loading wire:target="editType({{$client->id }})">
+                                            <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+                                            modification...
+                                        </span>
+                                    </button>
+                                    <button class="btn btn-raised btn-rounded btn-raised-danger"  wire:click="confirmerDelete({{$client->id }})">
+                                        <i class="nav-icon i-Close-Window font-weight-bold"></i>
+                                    </button>
+                                </td>
+                            </tr>                                  
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+
+                {{ $clients->links() }}
+
+            </div>
+        </div>
+    </div>
+    <!-- end of col-->
+    @endif
+</div>
+@push('scripts')
+    <script>
+        document.addEventListener("DOMContentLoaded", function(event) {
+            Livewire.on('removeNotification', function () {
+                setTimeout(function () {
+                    document.getElementById('notification').remove();
+                }, 1000);
+            });
+        });
+    </script>
+@endpush
