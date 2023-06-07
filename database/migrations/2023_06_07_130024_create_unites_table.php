f69bd17378514b6c65bf6034f129da6a17342729
Phrygien <84671612+phrygien@users.noreply.github.com>
2023-06-07 16:02:48 +0300

    model Unite GF-49

---
+++ database/migrations/2023_06_07_130024_create_unites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unites', function (Blueprint $table) {
+            $table->id();
+            $table->string('unite');
+            $table->string('label');
+            $table->integer('actif')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unites');
+    }
+}
